7c0e545da550e4a75b6b14026ff849206453cb0c
YQ Liang <13241620266@163.com>
2022-06-28 12:39:27 +0800

    add system encoding

--- Directory_Remark_win.py
+++ Directory_Remark_win.py
@@ -3,5 +3,15 @@
 # @Author   :yq-Liang
 # Good Luck !
 
+import sys
+import os
+
+#get the system encoding
+defEncoding=sys.getfilesystemencoding()
+
+#convert the code to system encoding
+def sysEncode(content):
+    pass
+
 if __name__=="__main__":
     print("it's ok.")
